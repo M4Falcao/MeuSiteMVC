fb9fa1bbbded6897fd5b1dedc8a3cf160c39c6eb
Mateus Falcão <mateus.souza.falcao@gmail.com>
2023-03-14 20:50:26 -0300

    Initial commit

---
+++ MeuSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MeuSiteMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel model = new HomeModel();
+            model.Nome = "Mateus Souza Falcão";
+            model.Email = "mateus.souza.falcao@gmail.com";
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
